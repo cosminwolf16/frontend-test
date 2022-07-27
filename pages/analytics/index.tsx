30cb4cbf05282c09b23b62a529f89a323029dacf
cosminwolf16 <clup_c@ngs.org>
2022-07-27 16:41:01 +0300

    set up Analytics page

---
+++ pages/analytics/index.tsx
@@ -0,0 +1,9 @@
+import styles from 'styles/Analytics.module.scss';
+
+export const Analytics = () => {
+  return (
+    <div>
+      <h1 className={styles.analyticsTitle}>Analytics page</h1>
+    </div>
+  );
+};
